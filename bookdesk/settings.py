717e16bb775d2bd0784aa0347e74682b418f65bb
Mayank Aggarwal <97796657+mayank-0407@users.noreply.github.com>
2023-01-29 23:03:23 +0530

    changing base url of settings

--- bookdesk/settings.py
+++ bookdesk/settings.py
@@ -134,7 +134,7 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = "mayank.tempdata@gmail.com"
 EMAIL_HOST_PASSWORD = "ambhhidvifqtonzs"
 
-BASE_URL_EMAIL="http://127.0.0.1:8000"
+BASE_URL_EMAIL="https://mayank21121.pythonanywhere.com/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
